2cb93700bccfaceddad897776429b184ba02c464
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-06-01 16:02:45 +0700

    feat(replace-string): replace existing package name and existing project name based on  user input

--- src/cli.js
+++ src/cli.js
@@ -5,9 +5,10 @@ var glob = require("glob")
 
 var appName = ""
 var packageName = ""
+
 const properties = [
     {
-        name: 'Input Package Name ',
+        name: 'packagename',
         validator: /^(?=.*[.])[a-z0-9_\.]+$/,
         warning: 'This field should be lower case and at least one "."'
     }
@@ -21,7 +22,7 @@ export function cli(args){
 	prompt.start();
 	prompt.get(properties, function (err, result) {
 		if (err) { return onErr(err); }
-		packageName = result.name
+		packageName = result.packagename
 		fetchGitRepo()
 	});
 }
@@ -32,24 +33,43 @@ function fetchGitRepo(){
 		console.log('Program output:', stdout);
 		console.log('Program stderr:', stderr);
 		if(code == 0){
-			executeProject()
+			getFilesInDirectory()
 		}
 	})
 }
 
-function executeProject(){
+function getFilesInDirectory(){
 	var getDirectories = function (src, callback) {
-		glob(src + '/**/*', callback);
+		glob(src + '/**/*.*', callback);
 	};
 	getDirectories('android-starterpack', function (err, res) {
 		if (err) {
 			console.log('Error', err);
 		} else {
-			console.log(res);
+			var paths = String(res).split(",")
+			for(var i = 0; i < paths.length ; i++){
+				renameFilesInDirectries(paths[i])
+			}
 		}
 	});
 }
 
+function renameFilesInDirectries(res) {
+	fs.readFile(res, 'utf8', function (err,data) {
+		if (err) {
+		  return console.log(err);
+		}
+		replaceValue(/com.godohdev.androidstarterkit/g, packageName, data, res)
+	 	replaceValue(/Android Starter Kit/g, appName, data, res)
+	  });
+}
+
+function replaceValue(pattern, value, data, res){
+	var result = data.replace(pattern, value);
+	fs.writeFile(res, result, 'utf8', function (err) {
+		if (err) return console.log(err);
+	});
+}
 function onErr(err) {
     console.log(err);
     return 1;
