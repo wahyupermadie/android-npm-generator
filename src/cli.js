5e2d71ff52832c9a5a833bc699aead16a83b79fa
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-06-01 14:00:36 +0700

    refactor(cli-js): add fetch repo and get files directory

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,10 @@
 const shell = require('shelljs')
 const prompt = require('prompt');
+const fs = require('fs');
+var glob = require("glob")
 
+var appName = ""
+var packageName = ""
 const properties = [
     {
         name: 'Input Package Name ',
@@ -10,14 +14,39 @@ const properties = [
 ];
 
 export function cli(args){
-	appName = process.argv.slice(2)[0]
+ 	appName = process.argv.slice(2)[0]
 	if(appName == null){
 		return onError("Your app name should not empyt !")
 	}
 	prompt.start();
 	prompt.get(properties, function (err, result) {
 		if (err) { return onErr(err); }
-		shell.exec('git clone https://github.com/wahyupermadie/android-starterpack.git')
+		packageName = result.name
+		fetchGitRepo()
+	});
+}
+
+function fetchGitRepo(){
+	shell.exec('git clone https://github.com/wahyupermadie/android-starterpack.git', function(code, stdout, stderr){
+		console.log('Exit code:', code);
+		console.log('Program output:', stdout);
+		console.log('Program stderr:', stderr);
+		if(code == 0){
+			executeProject()
+		}
+	})
+}
+
+function executeProject(){
+	var getDirectories = function (src, callback) {
+		glob(src + '/**/*', callback);
+	};
+	getDirectories('android-starterpack', function (err, res) {
+		if (err) {
+			console.log('Error', err);
+		} else {
+			console.log(res);
+		}
 	});
 }
 
